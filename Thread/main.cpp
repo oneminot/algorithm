5c425c52835cdec48bc7ed5fa51116055b4bd5f7
kushal <kushaldeveloper@gmail.com>
2015-03-29 13:57:52 -0500

    add a new project for maze portal

--- Thread/main.cpp
+++ Thread/main.cpp
@@ -4,26 +4,32 @@
 
 void foo()
 {
-	std::cout << "foo " << std::endl;
+	//std::cout << "foo " << std::endl;
 }
 
 void bar(int x)
 {
-	std::cout << "bar " << x << std::endl;
+	//std::cout << "bar " << x << std::endl;
+}
+
+void foobar()
+{
+	for (size_t i = 0; i < 10000000000000; i++)
+	{
+		std::thread first(foo);     // spawn new thread that calls foo()
+		std::thread second(bar, 0);  // spawn new thread that calls bar(0)
+
+		//std::cout << "main, foo and bar now execute concurrently...\n";
+
+		// synchronize threads:
+		first.join();                // pauses until first finishes
+		second.join();               // pauses until second finishes 
+	}
 }
 
 int main()
 {
-	std::thread first(foo);     // spawn new thread that calls foo()
-	std::thread second(bar, 0);  // spawn new thread that calls bar(0)
-
-	std::cout << "main, foo and bar now execute concurrently...\n";
-
-	// synchronize threads:
-	first.join();                // pauses until first finishes
-	second.join();               // pauses until second finishes
-
+	foobar();
 	std::cout << "foo and bar completed.\n";
-
 	return 0;
 }
